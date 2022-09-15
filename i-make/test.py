c1f03fe02669151deaa90435f94190f0273b4d76
GoRuGoo <dachanyr8989@gmail.com>
2022-09-15 16:26:10 +0900

    PR指摘点修正 #38

--- i-make/test.py
+++ i-make/test.py
@@ -19,39 +19,41 @@ def convert_rgba_to_rgb(image: np.ndarray, return_mask: bool) -> np.ndarray:
         return (image[:, :, :3] * np.dstack([mask / 255] * 3)).astype(np.uint8)
 
 
-def convert_make_color(
-    image: np.ndarray, h_ch: int, s_ch: int, v_ch: int, return_include_alphachannel: bool
+def convert_make_hsvcolor(
+    image: np.ndarray, hue: int, saturation: int, value: int, include_alpha_ch: bool
 ) -> np.ndarray:
     """指定したHSVにメイクを変更する.
 
     Args:
         image(_type_): B255で塗りつぶした透過メイク素材、1024x1024
-        h_ch(_type_):HSVのHの数値
-        s_ch(_type_):HSVのSの数値
-        v_ch(_type_):HSVのVの数値
-        return_include_alphachannel(_type_):returnする画像にアルファチャンネルを含むか否か
+        hue(_type_):HSVのHueの数値
+        saturation(_type_):HSVのSaturationの数値
+        value(_type_):HSVのVvalueの数値
+        include_alpha_ch(_type_):returnする画像にアルファチャンネルを含むか否か
+    Return:
+        np.array:任意の色、設定に変更したメイクのnumpy配列
     """
-    delete_alpha_image, mask = convert_rgba_to_rgb(image, True)
-    hsv = cv2.cvtColor(delete_alpha_image, cv2.COLOR_BGR2HSV)
+    image_wo_alpha, mask = convert_rgba_to_rgb(image, True)
+    image_hsv = cv2.cvtColor(image_wo_alpha, cv2.COLOR_BGR2HSV)
 
-    hsv[:, :, 0] = np.where(hsv[:, :, 0] == 120, h_ch / 2, hsv[:, :, 0])
-    hsv[:, :, 1] = np.where(hsv[:, :, 1] == 255, s_ch, hsv[:, :, 1])
-    hsv[:, :, 2] = np.where(hsv[:, :, 2] != 0, (v_ch * (hsv[:, :, 2] / 255)), hsv[:, :, 2])
+    image_hsv[:, :, 0] = np.where(image_hsv[:, :, 0] == 120, hue / 2, image_hsv[:, :, 0])
+    image_hsv[:, :, 1] = np.where(image_hsv[:, :, 1] == 255, saturation, image_hsv[:, :, 1])
+    image_hsv[:, :, 2] = np.where(image_hsv[:, :, 2] != 0, (value * (image_hsv[:, :, 2] / 255)), image_hsv[:, :, 2])
     # ↑グラデーションの比率を保ったまま、明度を変更する
 
-    bgr = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
+    image_bgr = cv2.cvtColor(image_hsv, cv2.COLOR_HSV2BGR)
 
-    if return_include_alphachannel:
-        b_ch, g_ch, r_ch = cv2.split(bgr[:, :, :3])
-        include_alpha_bgr_image = cv2.merge((b_ch, g_ch, r_ch, mask))
-        return include_alpha_bgr_image
+    if include_alpha_ch:
+        b_ch, g_ch, r_ch = cv2.split(image_bgr[:, :, :3])
+        image_bgr_w_alpha = cv2.merge((b_ch, g_ch, r_ch, mask))
+        return image_bgr_w_alpha
     else:
-        return bgr
+        return image_bgr
 
 
 image = cv2.imread("i-make/static/facepaints/B255.png", cv2.IMREAD_UNCHANGED)
 
-return_make = convert_make_color(image, 19, 255, 148, True)
+return_make = convert_make_hsvcolor(image, 240, 119.85, 163.2, True)
 cv2.imshow("test.png", return_make)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
