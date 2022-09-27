4b484676a7b6c35501e4ddcf720cce40e63ca4ca
kathmandu777 <mahhakatomanato@gmail.com>
2022-09-27 21:24:26 +0900

    feat: add mode icon image

--- i-make/mode/diagnosis.py
+++ i-make/mode/diagnosis.py
@@ -4,6 +4,8 @@ from ..mode.base import BaseModeEffect
 class DiagnosisMode(BaseModeEffect):
     """Diagnosis makeup mode."""
 
+    ICON_PATH: str = "i-make/static/facepaints/diagnosis/diagnosis.png"
+
     def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
 
