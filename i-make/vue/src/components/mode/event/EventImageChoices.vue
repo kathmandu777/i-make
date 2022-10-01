a55fb4d622e6a4c2b7eeff635d25e2a78026d3a2
kathmandu777 <mahhakatomanato@gmail.com>
2022-10-02 01:25:00 +0900

    feat: 10key ui

--- i-make/vue/src/components/mode/event/EventImageChoices.vue
+++ i-make/vue/src/components/mode/event/EventImageChoices.vue
@@ -1,12 +1,18 @@
 <template>
-<div>
-    <div class="container">
-        <label class="card" v-for="(choiceImagesPath, i) in choiceImagesPaths" :for="i" :key="i">
-            <input type="radio" :id="i" name="select-image" :value="choiceImagesPath" v-model="selectedChoiceImagesPath" v-on:change="confirm" />
-            <img v-bind:src="choiceImagesPath" width="230" height="250" />
-        </label>
+    <div>
+        <div class="container">
+            <div v-for="(choiceImagesPath, index) in currentChoiceImagesPaths" :key="index" :class="'key' + (index+1)">
+                <label class="card">
+                    <input type="radio" :id="index" :value="choiceImagesPath" v-model="selectedChoiceImagesPath"
+                        v-on:change="confirm" />
+                    <img :src="choiceImagesPath" width="230" height="230" />
+                </label>
+            </div>
+            <button class="key0 card" @click="goToMenu()">Menu</button>
+            <button class=" key-dot card" @click="setPage(page-1)">Back</button>
+            <button class=" key-enter card" @click="setPage(page+1)">Next</button>
+        </div>
     </div>
-</div>
 </template>
 
 <script>
@@ -16,6 +22,7 @@ export default {
         return {
             choiceImagesPaths: [],
             selectedChoiceImagesPath: "",
+            page: 0
         }
     },
     methods: {
@@ -26,6 +33,21 @@ export default {
             await window.eel.set_skin_color(27.0, 36.0, 100.0)() // TODO: 任意の色を指定できるように
             await window.eel.set_effect_image_from_path(this.selectedChoiceImagesPath)()
             await window.eel.start()
+        },
+        setPage(page) {
+            if (page<0)
+                page=0
+            else if (page>Math.floor((this.choiceImagesPaths.length-1)/9))
+                page=Math.floor((this.choiceImagesPaths.length-1)/9)
+            this.page=page
+        },
+        goToMenu() {
+            this.$router.push("/menu")
+        }
+    },
+    computed: {
+        currentChoiceImagesPaths() {
+            return this.choiceImagesPaths.slice(this.page*9, this.page*9+9)
         }
     },
     mounted: function () {
@@ -36,23 +58,27 @@ export default {
 
 <style scoped>
 .container {
-    width: 100%;
-    height: 100%;
-    padding: 10px;
-    display: flex;
-    flex-direction: row;
-    flex-wrap: wrap;
-    align-content: center;
-    justify-content: flex-start;
-    gap: 10px;
+    display: grid;
+    grid-template-columns: 240px 240px 240px 240px;
+    grid-template-rows: 60px 240px 240px 240px 240px 60px;
+    grid-template-areas:
+        ". . . ."
+        "key7 key8 key9 key-minus"
+        "key4 key5 key6 key-plus"
+        "key1 key2 key3 key-enter"
+        "key0 key0 key-dot key-enter"
+        ". . . .";
 }
 
 .card {
     border: 1px solid #ccc;
     border-radius: 5px;
+    display: block;
+    padding: 0;
+    margin: 5px;
 }
 
-.card > input {
+.card>input {
     display: none;
 }
 
@@ -60,4 +86,59 @@ export default {
     border: 2px solid #000;
 }
 
+.key0 {
+    grid-area: key0;
+}
+
+.key1 {
+    grid-area: key1;
+}
+
+.key2 {
+    grid-area: key2;
+}
+
+.key3 {
+    grid-area: key3;
+}
+
+.key4 {
+    grid-area: key4;
+}
+
+.key5 {
+    grid-area: key5;
+}
+
+.key6 {
+    grid-area: key6;
+}
+
+.key7 {
+    grid-area: key7;
+}
+
+.key8 {
+    grid-area: key8;
+}
+
+.key9 {
+    grid-area: key9;
+}
+
+.key-minus {
+    grid-area: key-minus;
+}
+
+.key-plus {
+    grid-area: key-plus;
+}
+
+.key-enter {
+    grid-area: key-enter;
+}
+
+.key-dot {
+    grid-area: key-dot;
+}
 </style>
