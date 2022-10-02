89c3024b2a35679fe706d104543af9eb50492cdb
kathmandu777 <mahhakatomanato@gmail.com>
2022-10-02 19:47:38 +0900

    chore: rename otegaru -> easy

--- i-make/mode/easy.py
+++ i-make/mode/easy.py
@@ -4,8 +4,8 @@ from ..mode.base import BaseModeEffect
 class EasyMode(BaseModeEffect):
     """Easy makeup mode."""
 
-    CHOICE_IMAGES_DIR_PATH = "i-make/static/facepaints/otegaru"  # TODO: otegaru -> easy
-    ICON_PATH: str = "i-make/static/facepaints/otegaru/otegaru.png"
+    CHOICE_IMAGES_DIR_PATH = "i-make/static/facepaints/easy"
+    ICON_PATH: str = "i-make/static/facepaints/easy/easy.png"
 
     def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
